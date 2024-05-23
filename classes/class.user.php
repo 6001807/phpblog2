0eb6c41f21a8c103b23c01a64e979f5d80de85f1
JEsper <6001807@mborijnland.nl>
2024-05-23 15:10:15 +0200

    last commit

--- classes/class.user.php
+++ classes/class.user.php
@@ -1,19 +1,26 @@
 <?php
+require_once('class.database.php');
 
 class User extends Database {
-    public $id;
+   
     public $name;
     public $password;
 
-    public function __construct() {
-
+    public function __construct($conn, $username, $password) {
+        parent::__construct($conn);
+        $this->username = $username;
+        $this->password = $password;
     }
 
     public function create($name, $password) {
-
+          
     }
 
     public function delete() {
+
+    }
+
+    public function logIn() {
 
     }
 }
