8c6a26483e529d02fa1bd51af16026f8cf2b9170
JespervnVeen <ninjahamstr@gmail.com>
2024-05-23 22:21:05 +0200

    data

--- classes/class.comment.php
+++ classes/class.comment.php
@@ -3,8 +3,13 @@ include_once 'class.database.php';
 
 class Comment {
     private $db;
+    private $name;
+    private $message;
 
-    public function __construct() {
+    public function __construct($name, $message) {
+        $this->name = $name;
+        $this->message = $message;
+        
         $this->db = new Database();
         $this->db->connect();
     }
--- classes/class.posts.php
+++ classes/class.posts.php
@@ -3,9 +3,14 @@ include_once 'class.database.php';
 
 class Post {
     private $db;
+    private $title;
+    private $description;
+    private $text;
 
-    public function __construct() {
-
+    public function __construct($title, $description, $text) {
+        $this->title = $title;
+        $this->description = $description;
+        $this->text = $text;
         
         $this->db = new Database();
         $this->db->connect();
