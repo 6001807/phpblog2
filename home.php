f3f2088f7ebb90b15dd1e5254d6192cea0ca1029
JespervnVeen <ninjahamstr@gmail.com>
2024-05-24 19:39:57 +0200

    7u8

--- home.php
+++ home.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel='stylesheet' href='./styles.css'>
 </head>
 <?php
     include_once 'classes/class.posts.php';
@@ -12,20 +13,22 @@
     session_start();
 ?>
 <body>
-    <?php if(isset($_SESSION['role_id'])) { ?>
-        <?php if($_SESSION['role_id'] == 1) { ?>
+    <header>
+        <?php if(isset($_SESSION['role_id'])) { ?>
+            <?php if($_SESSION['role_id'] == 1) { ?>
+                <section class='link'>
+                    <p><a href="../phpblog2/admin.php">Open admin panel</a></p>
+                </section>
+            <?php } ?>
+            <form method="post" action="process.php"> 
+                <button type="submit" name="logout">Logout</button>
+            </form>
+        <?php } else { ?>
             <section class='link'>
-                <p><a href="../phpblog2/admin.php">Open admin panel</a></p>
+                    <p><a href="login.php">Login</a></p>
             </section>
         <?php } ?>
-        <form method="post" action="process.php"> 
-            <button type="submit" name="logout">Logout</button>
-        </form>
-    <?php } else { ?>
-        <section class='link'>
-                <p><a href="login.php">Login</a></p>
-        </section>
-    <?php } ?>
+    </header>
     <section id='post-list'>
         <?php foreach($allPosts as $post) { ?>
             <section class="post-item">
